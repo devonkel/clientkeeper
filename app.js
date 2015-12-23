acccec8e3b580a0d4f23c1f761eaa8a95486f821
Devon Kelly <devon.kelly3@gmail.com>
2015-12-23 14:27:26 -0600

    added Edit functionality

--- app.js
+++ app.js
@@ -11,14 +11,38 @@ app.use(bodyParser.json());
 
 app.get('/clients', function (req, res){
 	console.log('Request for clients received');
-	
-	db.clients.find( function (err, docs) {
+
+	db.clients.find().sort({last_name: 1}, function (err, docs) {
 		if(err) {
 			console.log('---ERROR getting clients');
 			res.send(err);
 		} else {
 			console.log('Sending Data ...');
 			res.json(docs);
+		}
+	});
+});
+
+app.post('/clients', function (req, res){
+	db.clients.insert(req.body, function(err, doc){
+		if(err) {
+			console.log('---ERROR adding client');
+			res.send(err);
+		} else {
+			console.log('Client added.');
+			res.json(doc);
+		}
+	});
+});
+
+app.get('/clients/:id', function (req, res){
+	var id = req.params.id;
+
+	db.clients.findOne({_id: mongojs.ObjectId(id)}, function (err, doc){
+		if(err) {
+			res.send(err);
+		} else {
+			res.json(doc);
 		}
 	});
 });
--- public/controllers/AppController.js
+++ public/controllers/AppController.js
@@ -1,8 +1,25 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('AppController', ['$scope', '$http', '$location', function($scope, $http, $location){
-	$http.get('/clients').success(function(response){
+myApp.controller('AppController', ['$scope', '$http', '$location', function ($scope, $http, $location){
+	$http.get('/clients').success(function (response){
 		console.log('Data recieved from the server');
 		$scope.clients = response;
 	});
+
+	$scope.addClient = function (){
+		console.log('Adding new client...');
+		$http.post('/clients', $scope.client).success(function (response){
+			console.log('Client Added');
+			window.location.href = '/';
+		});
+	};
+
+	$scope.editClient = function (id){
+		console.log('Editing client...');
+		$('#addBtn').remove();
+		// $('#updateBtn').show();
+		$http.get('/clients/'+id).success(function (response){
+			$scope.client = response;
+		});
+	};
 }]);
